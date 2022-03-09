a2061a47ae1504e48466e079689183d794be9dc8
hudayaak <houdayaakoubi1@gmail.com>
2022-03-09 21:08:59 +0000

    Updated Login Screen and implemented Firebase Authentication
    Here as well a lot of edits has been as I wasn't able to implement Firebase Authentication using the Formik form library. Therefore, I decided to remove Formik form and instead use style objects using Stylesheet.

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,105 +1,183 @@
-import React, {useState} from 'react';
-import {
-    StyledContainer,
-    InnerContainer,
-    PageLogo,
-    PageTitle,
-    StyledFormArea,
-    LeftIcon,
-    RightIcon,
-    StyledInputLabel,
-    StyledTextInput,
-    StyledButton, 
-    Colors,
-    ButtonText,
-    ExtraView,
-    ExtraText,
-    TextLink,
-    TextLinkContent
-} from '../components/styles';
-import { Formik } from 'formik';
-import { View, Text, Image, StyleSheet } from 'react-native';
-import {Octicons,Ionicons} from '@expo/vector-icons';
-import KeyboardAvoidingWrapper from '../components/KeyboardAvoidingWrapper';
+import React, {useState, useEffect} from 'react'
+import { StyleSheet, Text, TextInput, View, TouchableOpacity, Image } from 'react-native';
+import { Colors } from '../components/styles';
+import KeyboardAvoidingWrapper from '../components/KeyboardAvoidingWrapper'
+import { auth } from '../Firebase/firebase'
 
-const {brand, darkLight} = Colors;
+
+const {primary, black, secondary,darkLight, brand} = Colors;
 
 
 const Login = ({navigation}) => {
-    const [hidePassword, setHidePassword] = useState(true);
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState(''); 
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            if (user) {
+                navigation.replace("Account")
+
+            }
+        })
+        return unsubscribe
+    }, [])
+
+    const handleSignIn = () => {
+        auth
+        .signInWithEmailAndPassword(email, password)
+        .then(userCredentials => {
+            const user = userCredentials.user;
+            console.log("Logged in with:", user.email)
+        })
+        .catch(error => alert(error.message))
+    }
+
+    const MainContainer = View;
+    const Container = View;
     const Title = Text;
     const Logo = Image;
+    const StyledFormArea = View;
+    const StyledButton = TouchableOpacity;
+    const ButtonText = Text;
+    const ExtraView = View;
+    const ExtraText = Text;
+    const TextLink = TouchableOpacity;
+    const TextLinkContent = Text;
 
     return (
-        <KeyboardAvoidingWrapper><StyledContainer>
-            <InnerContainer>
+        <MainContainer style={styles.InnerContainer}><KeyboardAvoidingWrapper>
+            <Container style={styles.LoginContainer}>
                 <Logo style={styles.LogoLogin}resizeMode="cover" source={require('../../assets/images/logo.png')}/>
                 <Title style={styles.PageTitleLogin} >Account Login</Title>
+                <StyledFormArea>
                 
-                <Formik
-                    initialValues={{email: '', password: ''}}
-                    onSubmit={(values) => {
-                        console.log(values);
-                        navigation.navigate("Account")
-                    }}
-                    >{({handleChange, handleBlur, handleSubmit, values}) => <StyledFormArea>
-                        <MyTextInput label="Email Address" icon="mail" onChangeText={handleChange('email')} 
-                         onBlur={handleBlur('email')} value={values.email} keyboardType="email-address"
-                         placeholder="*****@gmail.com" placeholderTextColor={darkLight}/>
-
-                        <MyTextInput label="Password" icon="lock" placeholder="*********" placeholderTextColor={darkLight} onChangeText={handleChange('password')} 
-                        onBlur={handleBlur('password')} value={values.password} secureTextEntry={hidePassword}
-                        isPassword={true} 
-                        hidePassword={hidePassword}
-                        setHidePassword={setHidePassword}/>
-                        <StyledButton onPress={handleSubmit}>
-                            <ButtonText>Login</ButtonText>
-                        </StyledButton>
-                        <ExtraView>
-                            <ExtraText>Don't have an Account? </ExtraText>
-                                <TextLink onPress={() => navigation.navigate("Signup")}>
-                                <TextLinkContent>Signup</TextLinkContent>
+                <View>
+                    
+                <TextInput
+                  placeholder='Email Address'
+                  value={email}
+                  onChangeText={text => setEmail(text)}
+                  style={styles.input}
+                  />
+                  <TextInput
+                  placeholder='Password'
+                  value={password}
+                  onChangeText={text => setPassword(text)}
+                  style={styles.input}
+                  secureTextEntry
+                  />
+                  </View>
+                  <View style={styles.ButtonContainer}>
+                  <StyledButton style={styles.Button} 
+                     onPress={handleSignIn}>
+                         <ButtonText style={styles.Text}>Login</ButtonText>
+                  </StyledButton>
+                        </View>
+                        <ExtraView style={styles.extraView}>
+                            <ExtraText style={styles.extraText}>Don't have an Account? </ExtraText>
+                                <TextLink style={styles.textLink} onPress={() => navigation.navigate("Signup")}>
+                                <TextLinkContent style={styles.textLinkContent}>Signup</TextLinkContent>
                             </TextLink>
                         </ExtraView>
-                    </StyledFormArea> }
-                </Formik>
-            </InnerContainer>
-        </StyledContainer>
+                </StyledFormArea>
+                </Container>
         </KeyboardAvoidingWrapper>
-    );
-};
+        </MainContainer>
+    )
+}
 
-const MyTextInput = ({label, icon, isPassword, hidePassword, setHidePassword, ...props}) => {
-    return (
-        <View>
-            <LeftIcon>
-                <Octicons name={icon} size={30} color={brand} />
-            </LeftIcon>
-            <StyledInputLabel>{label}</StyledInputLabel>
-            <StyledTextInput {...props} />
-            {isPassword && (
-                <RightIcon onPress={()=> setHidePassword(!hidePassword)}>
-                    <Ionicons name={hidePassword ? 'md-eye-off' :'md-eye'} size={30} color={darkLight}/>
-                </RightIcon>
-            )}
-        </View>
-    );
-
-};
 
 const styles = StyleSheet.create({
+
+    InnerContainer: {
+        flex:1,
+        padding : 25,
+        width: '100%',
+        alignContent: 'center',
+        color: primary,
+        backgroundColor: primary
+    },
+    
+    LoginContainer: {
+        padding: 25,
+        paddingTop: 30,
+        justifyContent: 'center',
+        backgroundColor: primary
+    },
+
+    input: {
+        height:50,
+        backgroundColor: secondary,
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5,
+    },
+
     PageTitleLogin: {
         fontSize: 30,
-        alignContent: 'center',
+        textAlign: 'center',
         fontWeight: 'bold',
         color: '#2c2f36',
-        padding: 10
+        padding: 10,
+        paddingTop: 10
     },
     LogoLogin: {
         width: 250,
         height: 200,
 
+    },
+
+    ButtonContainer: {
+        width: '50%',
+        alignContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
+        paddingLeft: 135
+        
+    },
+
+    Button: {
+
+        width: 270,
+        backgroundColor: '#e69557',
+        justifyContent: 'center',
+        alignContent: 'center',
+        borderRadius: 10,
+        marginVertical: 5,
+        height: 55,
+        alignItems: 'center',
+    }, 
+
+    Text: {
+        fontSize: 16,
+        color:primary
+    },
+    
+    extraView: {
+        justifyContent: 'center',
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+    },
+    
+    extraText: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: black,
+        fontSize: 15,
+    },
+    
+    textLink: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    
+    textLinkContent: {
+        color: brand,
+        fontSize: 15
     }
-});
+})
+
 export default Login;
