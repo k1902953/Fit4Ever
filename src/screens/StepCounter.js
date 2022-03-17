4b03d847020c0532b9dd8e2f7c63763905b8b4d0
hudayaak <houdayaakoubi1@gmail.com>
2022-03-17 18:07:44 +0000

    Updated StepCounter.js
    Added and styled Circular progress bar showing the number of steps taken and the progress to the set target

--- src/screens/StepCounter.js
+++ src/screens/StepCounter.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import {StyleSheet, Text, View, Dimensions,} from "react-native";
 import { Pedometer } from "expo-sensors";
-
+import CircularProgress from "react-native-circular-progress-indicator";
 
 const StepCounter = ({navigation}) =>{
 
@@ -37,7 +37,25 @@ const StepCounter = ({navigation}) =>{
                     REACH YOUR DAILY STEP COUNT !
                 </Text>
             </View>
-        </View>
+            <View style={{ flex: 3 }}>
+                <CircularProgress
+                value={StepCount}
+                maxValue={6500}
+                radius={185}
+                textColor={"#ecf0f1"}
+                activeStrokeColor={"#f39c12"}
+                inActiveStrokeColor={'#e69557'}
+                inActiveStrokeOpacity={0.5}
+                inActiveStrokeWidth={40}
+                activeStrokeWidth={40}
+                title={"Step Count"}
+                titleColor={"#ecf0f1"}
+                titleStyle={{ fontWeight: "bold" }}
+                titleFontSize={40}
+                />
+       </View>
+
+       </View>
         );
 
 }
